bd5e2a6a1fe128c084516ecfd93ce4570237b35a
GoDillonAudris512 <110383663+GoDillonAudris512@users.noreply.github.com>
2023-11-23 18:54:18 +0700

    chore: some template for implementation using library

---
+++ src/comparison.py
@@ -0,0 +1,9 @@
+# File to implement KNN and Naive Bayes Algorithm using scikit-learn
+# This file will not be integrated with main.py and stand as its own
+import pandas as pd
+
+# KNN Implementation
+# Write here
+
+# Naive Bayes Implementation
+# Write here
